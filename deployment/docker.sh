0caad1436c51f64ef5d37338021ef9b0f559c7eb
mibarg <mibarg@users.noreply.github.com>
2019-07-11 23:26:15 +0300

    Test docker after build

--- deployment/docker.sh
+++ deployment/docker.sh
@@ -8,6 +8,7 @@ if [[ "$#" -ne 2 ]]; then
 fi
 if [[ $2 = "build" ]]; then
    docker build -t $1 -f deployment/Dockerfile .
+   docker run -it $1 python3 -m pytest PySeal/tests/
 elif [[ $2 = "run" ]]; then
    docker run -it $1 python3
 elif [[ $2 = "test" ]]; then
