f3748d7bd61dfc0ae313544fd6b67a900fc6d351
mibarg <mibarg@users.noreply.github.com>
2019-07-12 16:41:47 +0300

    basic tests

---
+++ tests/scheme_test.py
@@ -0,0 +1,18 @@
+import pytest
+from itertools import product
+
+from seal.models import CipherScheme
+
+
+@pytest.mark.parametrize("poly_mod, coeff_mod, plain_mod, security", product([2**11, 2**12, 2**13, 2**14], [-1, 0, 0x7fffffffba0001, 0x7fffffffaa0001], [256, 293], [128, 192]))
+def test_eq(poly_mod, coeff_mod, plain_mod, security):
+    cs1 = CipherScheme(poly_mod, coeff_mod, plain_mod, security)
+    cs2 = CipherScheme(poly_mod, coeff_mod, plain_mod, security)
+
+    assert cs1 == cs2
+
+
+@pytest.mark.parametrize("poly_mod, coeff_mod, plain_mod, security", product([2**11, 2**12, 2**13, 2**14], [-1, 0, 0x7fffffffba0001, 0x7fffffffaa0001], [256, 293], [128, 192]))
+def test_str(poly_mod, coeff_mod, plain_mod, security):
+    cs = CipherScheme(poly_mod, coeff_mod, plain_mod, security)
+    _ = str(cs)
