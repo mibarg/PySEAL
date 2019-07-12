345994f86fc7006e67f142a1b6ad991a0b0d7cda
mibarg <mibarg@users.noreply.github.com>
2019-07-12 16:40:34 +0300

    CipherText, CipherScheme classes

--- seal/models.py
+++ seal/models.py
@@ -2,6 +2,7 @@ from typing import Union, Tuple, Type
 import logging
 
 from seal._primitives import *
+from seal.utils import is_pow_of_two
 
 
 class Encoder:
@@ -50,3 +51,109 @@ class Encoder:
             # TODO non int 32
             return self._encoder.decode_int32(encoded)
         return NotImplemented
+
+
+class CipherText:
+    def __init__(self, cipher: Ciphertext, evl: Evaluator, encoder: Encoder):
+        self._cipher = cipher
+        self._evl = evl
+        self._encoder = encoder
+
+    def __add__(self, other):
+        if isinstance(other, CipherText) and self._encoder == other._encoder:
+            res = Ciphertext()
+            self._evl.add(self._cipher, other._cipher, res)
+            return CipherText(res, self._evl, self._encoder)
+        else:
+            return NotImplemented
+
+    def __neg__(self):
+        res = Ciphertext()
+        self._evl.negate(self._cipher, res)
+        return CipherText(res, self._evl, self._encoder)
+
+    def __sub__(self, other):
+        if isinstance(other, CipherText) and self._encoder == other._encoder:
+            return other + (-self)
+        else:
+            return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, CipherText) and self._encoder == other._encoder:
+            res = Ciphertext()
+            self._evl.multiply(self._cipher, other._cipher, res)
+            return CipherText(res, self._evl, self._encoder)
+        else:
+            return NotImplemented
+
+
+class CipherScheme:
+    def __init__(self,
+                 poly_mod: int = 2048, coeff_mod: int = 0,
+                 plain_mod: int = 256, security: int = 128):
+
+        try:
+            params = EncryptionParameters()
+
+            # Security
+            assert security in (128, 192)
+
+            # Polynomial modulus
+            assert is_pow_of_two(poly_mod)
+            params.set_poly_modulus("1x^%d + 1" % poly_mod)
+
+            # Coefficient modulus
+            assert isinstance(coeff_mod, int)
+            if coeff_mod <= 0:
+                # Automatically choose by SEAL recommendations
+                coeff_mod_func = {128: coeff_modulus_128, 192: coeff_modulus_192}
+                params.set_coeff_modulus(coeff_mod_func[security](poly_mod))
+
+            # Plaintext modulus
+            assert isinstance(plain_mod, int)
+            params.set_plain_modulus(plain_mod)
+
+            self._context = SEALContext(params)
+        except AssertionError as e:
+            raise ValueError("Illegal parameters, {}".format(e))
+
+    def generate_keys(self):
+        key_gen = KeyGenerator(self._context)
+        pk = key_gen.public_key()
+        sk = key_gen.secret_key()
+        return pk, sk
+
+    def encrypt(self, pk: PublicKey, plain: Union[int, float], base: int):
+        encoded, encoder = Encoder.from_plain(plain, self._context.poly_modulus(), base)
+
+        cipher = Ciphertext()
+        Encryptor(self._context, pk).encrypt(encoded, cipher)
+
+        return CipherText(cipher, Evaluator(self._context), encoder)
+
+    # noinspection PyProtectedMember
+    def decrypt(self, sk: SecretKey, cipher: CipherText):
+        plain = Plaintext()
+        Decryptor(self._context, sk).decrypt(cipher._cipher, plain)
+
+        decoded = cipher._encoder.decode(plain)
+        return decoded
+
+    # noinspection PyProtectedMember
+    def noise_budget(self, sk: SecretKey, cipher: CipherText):
+        dec = Decryptor(self._context, sk)
+        return dec.invariant_noise_budget(cipher._cipher)
+
+    def __str__(self) -> str:
+        return "CipherScheme(poly_mod={}, coeff_mod_size={} bits, plain_mod={}, noise_std={})".format(
+            self._context.poly_modulus(), self._context.total_coeff_modulus().significant_bit_count(),
+            self._context.plain_modulus().value(), self._context.noise_standard_deviation())
+
+    def __eq__(self, other) -> bool:
+        if (isinstance(other, CipherScheme)
+                and self._context.poly_modulus().coeff_count() == other._context.poly_modulus().coeff_count()
+                and self._context.total_coeff_modulus().significant_bit_count() == other._context.total_coeff_modulus().significant_bit_count()
+                and self._context.plain_modulus().value() == other._context.plain_modulus().value()):
+            return True
+        else:
+            return False
