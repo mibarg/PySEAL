888634a1bb9c4b6bcd334a4e45b0453e5e793f0d
mibarg <mibarg@users.noreply.github.com>
2019-07-14 00:15:39 +0300

    Update import to sealed name refactoring

--- tests/scheme_test.py
+++ tests/scheme_test.py
@@ -1,7 +1,7 @@
 import pytest
 from itertools import product
 
-from seal.models import CipherScheme
+from sealed.models import CipherScheme
 
 
 @pytest.mark.parametrize("poly_mod, coeff_mod, plain_mod, security", product([2**11, 2**12, 2**13, 2**14], [-1, 0, 0x7fffffffba0001, 0x7fffffffaa0001], [256, 293], [128, 192]))
