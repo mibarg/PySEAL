d57a688501e7f3546c510bc6d97ac3f5d3128cb8
mibarg <mibarg@users.noreply.github.com>
2019-07-14 11:13:59 +0300

    remove redundant test cases

--- tests/scheme_test.py
+++ tests/scheme_test.py
@@ -4,7 +4,7 @@ from itertools import product
 from sealed.models import CipherScheme
 
 
-@pytest.mark.parametrize("poly_mod, coeff_mod, plain_mod, security", product([2**11, 2**12, 2**13, 2**14], [-1, 0, 0x7fffffffba0001, 0x7fffffffaa0001], [256, 293], [128, 192]))
+@pytest.mark.parametrize("poly_mod, coeff_mod, plain_mod, security", product([2**11, 2**12, 2**13, 2**14], [0, 0x7fffffffba0001, 0x7fffffffaa0001], [256, 293], [128, 192]))
 def test_eq(poly_mod, coeff_mod, plain_mod, security):
     cs1 = CipherScheme(poly_mod, coeff_mod, plain_mod, security)
     cs2 = CipherScheme(poly_mod, coeff_mod, plain_mod, security)
@@ -12,14 +12,14 @@ def test_eq(poly_mod, coeff_mod, plain_mod, security):
     assert cs1 == cs2
 
 
-@pytest.mark.parametrize("poly_mod, coeff_mod, plain_mod, security", product([2**11, 2**12, 2**13, 2**14], [-1, 0, 0x7fffffffba0001, 0x7fffffffaa0001], [256, 293], [128, 192]))
+@pytest.mark.parametrize("poly_mod, coeff_mod, plain_mod, security", product([2**11, 2**12, 2**13, 2**14], [0, 0x7fffffffba0001, 0x7fffffffaa0001], [256, 293], [128, 192]))
 def test_str(poly_mod, coeff_mod, plain_mod, security):
     cs = CipherScheme(poly_mod, coeff_mod, plain_mod, security)
 
     _ = str(cs)
 
 
-@pytest.mark.parametrize("poly_mod, coeff_mod, plain_mod, security", product([2**11, 2**12, 2**13, 2**14], [-1, 0, 0x7fffffffba0001, 0x7fffffffaa0001], [256, 293], [128, 192]))
+@pytest.mark.parametrize("poly_mod, coeff_mod, plain_mod, security", product([2**11, 2**12, 2**13, 2**14], [0, 0x7fffffffba0001, 0x7fffffffaa0001], [256, 293], [128, 192]))
 def test_key_generation(poly_mod, coeff_mod, plain_mod, security):
     cs = CipherScheme(poly_mod, coeff_mod, plain_mod, security)
 
@@ -31,9 +31,8 @@ def test_key_generation(poly_mod, coeff_mod, plain_mod, security):
 def test_fresh_noise_budget(coeff_mod, plain_mod, expected_noise,
                             poly_mod=2048, security=128, plain=1, base=2):
     cs = CipherScheme(poly_mod, coeff_mod, plain_mod, security)
-
     pk, sk = cs.generate_keys()
-    cipher = cs.encrypt(pk, plain, base)
 
+    cipher = cs.encrypt(pk, plain, base)
     # Noise budget in bits ~ log2(coeff_modulus/plain_modulus)
     assert expected_noise == cs.noise_budget(sk, cipher)
