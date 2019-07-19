ef424a1b611cd9315faa593868f5754407a92de0
mibarg <mibarg@users.noreply.github.com>
2019-07-19 11:57:06 +0300

    Add parameter to run a specific test

--- deployment/docker.sh
+++ deployment/docker.sh
@@ -2,19 +2,19 @@
 if [[ "${PWD##*/}" = "deployment" ]]; then
     cd ..
 fi
-if [[ "$#" -ne 2 ]]; then
-    echo "Must provide two arguments: container-name, command [build, run, test]"
+if [[ "$#" < 2 ]]; then
+    echo "Must provide at least two arguments: container-name, command [build, run, test]"
     exit 1
 fi
 if [[ $2 = "build" ]]; then
-   docker build -t $1 -f deployment/Dockerfile .
+    docker build -t $1 -f deployment/Dockerfile .
 elif [[ $2 = "build-and-test" ]]; then
-   ./$0 $1 build
-   ./$0 $1 test
+    ./$0 $1 build
+    ./$0 $1 test
 elif [[ $2 = "run" ]]; then
-   docker run -it $1 python3
+    docker run -it $1 python3
 elif [[ $2 = "test" ]]; then
-   docker run -it $1 python3 -m pytest PySeal/tests/
+    docker run -it $1 python3 -m pytest PySeal/tests/$3
 else
   echo "Incorrect command argument. Choices: build, run, test"
 fi
